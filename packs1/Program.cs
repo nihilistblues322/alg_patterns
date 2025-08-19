f1fca4547ff3e6c4867309b3193ef5d50fa4ce33
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-08-19 12:57:10 +0500

    next

--- packs1/Program.cs
+++ packs1/Program.cs
@@ -13,12 +13,31 @@ class Program
         Console.WriteLine(FindMinMax([5, 6, 8, 11, 23, 45, 9, 2]));
         Console.WriteLine(SumArray([5, 6, 8, 11, 23, 45, 9, 2]));
 
-        var arr = new[] { 1, 2, 3, 4, 5 };
-        ReverseArray(arr);
-        foreach (var i in arr)
-        {
-            Console.Write($"{i}, ");
-        }
+        // var arr = new[] { 1, 2, 3, 4, 5 };
+        // ReverseArray(arr);
+        // foreach (var i in arr)
+        // {
+        //     Console.Write($"{i}, ");
+        // }
+
+
+        // var arr1 = new[] { 1, 2, 3, 4, 5 };
+        // MoveLeft(arr1);
+        // foreach (var i in arr1)
+        // {
+        //     Console.Write($"{i}, ");
+        // }
+
+
+        // var arr1 = new[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+        // var res = RemoveNulls(arr1);
+        // foreach (var i in res)
+        // {
+        //     Console.Write($"{i}, ");
+        // }
+
+        Console.WriteLine(IsPal("level"));
+        Console.WriteLine(Reverse2("bombordini"));
     }
 
     static int Sum(int input)
@@ -102,5 +121,96 @@ class Program
             left++;
             right--;
         }
+    }
+
+    static void MoveLeft(int[] array)
+    {
+        int first = array[0];
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+
+        array[array.Length - 1] = first;
+    }
+
+    static void MoveRight(int[] array)
+    {
+        int last = array[array.Length - 1];
+
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            array[i] = array[i - 1];
+        }
+
+        array[0] = last;
+    }
+
+
+    static int[] RemoveNulls(int[] array)
+    {
+        int count = 0;
+        foreach (var t in array)
+            if (t != 0)
+                count++;
+
+        int[] result = new int[count];
+
+        int idx = 0;
+        foreach (var t in array)
+        {
+            if (t != 0)
+            {
+                result[idx] = t;
+                idx++;
+            }
+        }
+
+        return result;
+        return array.Where(t => t != 0).ToArray();
+    }
+
+
+    static bool IsPal(string input)
+    {
+        var chars = input.ToLower().Where(char.IsLetterOrDigit).ToArray();
+
+        int start = 0;
+        int end = chars.Length - 1;
+
+        while (start < end)
+        {
+            if (chars[start] != chars[end])
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    static string Reverse(string input)
+    {
+        var chars = input.ToCharArray();
+
+        int start = 0;
+        int end = chars.Length - 1;
+
+        while (start < end)
+        {
+            (chars[start], chars[end]) = (chars[end], chars[start]);
+
+            start++;
+            end--;
+        }
+
+        return new string(chars);
+    }
+
+    static string Reverse2(string input)
+    {
+        return new string(input.ToLower().Reverse().ToArray());
     }
 }
