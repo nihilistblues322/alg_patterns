91b534a0543897fe3ac6338837bd289d50748392
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-09-01 11:26:02 +0500

    next

--- arrayss_sql/Program.cs
+++ arrayss_sql/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //  1-------------------------------------------------------
+        var clientsA = new int[] { 1, 2, 3, 4, 5 };
+        var clientsB = new int[] { 6, 7, 8, 9, 10 };
+
+        var clientsAstring = new string[] { "a", "b", "c", "d" };
+        var clientsBstring = new string[] { "z", "x", "v", "g" };
+
+
+        PrintArray(IMerge(clientsA, clientsB));
+        PrintArray(DMerge(clientsA, clientsB));
+
+        PrintArray(IMerge(clientsAstring, clientsBstring));
+        PrintArray(DMerge(clientsAstring, clientsBstring));
+        //-------------------------------------------------------
+    }
+
+    // int ---------------------------------
+    static List<T> IMerge<T>(T[] a, T[] b)
+    {
+        var result = new List<T>();
+
+        foreach (var c in a)
+        {
+            result.Add(c);
+        }
+
+        foreach (var c in b)
+        {
+            result.Add(c);
+        }
+
+        return result;
+    }
+
+    static List<T> DMerge<T>(T[] a, T[] b)
+    {
+        var res = a.Concat(b).ToList();
+
+        return res;
+    }
+
+    // ----------------------------------------------------------
+
+
+    static void PrintArray(List<int> array)
+    {
+        if (array.Count == 0)
+        {
+            Console.WriteLine("[]");
+            return;
+        }
+
+        Console.WriteLine($"[{string.Join(", ", array)}]");
+    }
+
+    static void PrintArray(List<string> array)
+    {
+        if (array.Count == 0)
+        {
+            Console.WriteLine("[]");
+            return;
+        }
+
+        Console.WriteLine($"[{string.Join(", ", array)}]");
     }
 }
