4cdcdd406e4c4abccda2b29a2d4375df5c2e3842
AD <dimash.abdrakhmanov@brokeree.com>
2025-10-11 15:04:51 +0500

    has dup

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ContainsDuplicate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new[] { 1, 2, 3, 3 };
+        var result = HasDuplicate(nums);
+        Console.WriteLine(result);
+    }
+
+    static bool HasDuplicate(int[] nums)
+    {
+        var hash = new HashSet<int>();
+
+        foreach (var i in nums)
+        {
+            if (hash.Contains(i))
+            {
+                return true;
+            }
+
+            hash.Add(i);
+        }
+
+        return false;
+    }
+
+    static bool HasDuplicate2(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
